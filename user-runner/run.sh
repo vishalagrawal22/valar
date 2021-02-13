bdb448a98724c64cada682772282473cf0957626
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-02-14 00:50:37 +0530

    yet another responsive change

--- user-runner/run.sh
+++ user-runner/run.sh
@@ -1,6 +1,6 @@
 g++ correctcode.cpp -o correctcode
 g++ usercode.cpp -o usercode
-timeout 5s bash ./runcode.sh
+timeout 20s bash ./runcode.sh
 if [ $? -eq 124 ]; then 
     echo "Time Limit Exceeded" > verdict.txt
 fi
