e795d6591990fc5d753d46e7b6e71951398b64ce
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-02-13 20:39:34 +0530

    change verdict statements

--- user-runner/run.sh
+++ user-runner/run.sh
@@ -2,5 +2,5 @@ g++ correctcode.cpp -o correctcode
 g++ usercode.cpp -o usercode
 timeout 5s bash ./runcode.sh
 if [ $? -eq 124 ]; then 
-    echo "TLE on latest testcase. You can see the latest testcase in the testcase.txt file"
+    echo "TLE"
 fi
--- user-runner/runcode.sh
+++ user-runner/runcode.sh
@@ -3,19 +3,20 @@ n=10
 for((i = 1; i <= n; ++i)); do
     python3 tcgen.py > testcase.txt
     ./correctcode < testcase.txt > correct_output.txt
-    start=$(date +%s.%N)
+    # start=$(date +%s.%N)
     ./usercode < testcase.txt > user_output.txt
-    dur=$(echo "$(date +%s.%N) - $start" | bc)
-    printf "Execution time: %.3f s" $dur
-    echo
+    # dur=$(echo "$(date +%s.%N) - $start" | bc)
+    # printf "Execution time: %.3f s" $dur
+    # echo
     diff -i user_output.txt correct_output.txt > /dev/null || break
-    echo "Passed test: "  $i
-    echo "------------------------------"
+    # echo "Passed test: "  $i
+    # echo "------------------------------"
     j=$((j+1))
     # echo $j
 done
 if [ $j != $n ] 
 then 
-    echo "Wrong answer on current testcase :(. You can see the testcase in the testcase.txt file."
+    echo "Wrong"
 fi 
+echo "Correct"
 #total runtime of 10 testcases = 1s
